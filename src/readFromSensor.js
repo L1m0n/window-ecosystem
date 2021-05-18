dcff45306a1a12aab05b4e6761242b1ad37ddaa0
L1m0n <l1m0n3zon@gmail.com>
2021-05-18 20:01:32 +0300

    added readFromSensor

--- src/readFromSensor.js
+++ src/readFromSensor.js
@@ -1,6 +1,6 @@
-const AM2330 = import('../am2320');
+const AM2330 = import('../am2320/index.js');
 import BH1750 from 'bh1750';
-console.log(BH1750);
+console.log(AM2330);
 const light = new BH1750();
 
 const BUS_NUMBER = 1;
